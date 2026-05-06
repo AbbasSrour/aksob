8768f4427b2ed56ac388e0d80d16a825263e92cd
Abbas Srour <abbas.mj.srour@gmail.com>
2026-05-06 20:24:56 +0300

    Update `api.ts` to use `VITE_APP_URL` with `/api` suffix for `apiBaseUrl`

--- apps/client/lib/api.ts
+++ apps/client/lib/api.ts
@@ -1,7 +1,7 @@
 import { createApiClient } from "@aksob/sdk";
 import { env } from "@/config/env.ts";
 
-export const apiBaseUrl = env.VITE_API_URL;
+export const apiBaseUrl = `${env.VITE_APP_URL}/api`;
 
 export const api = createApiClient(apiBaseUrl, {
 	onResponse: async (response) => {
